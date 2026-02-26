c4ef387ba2aaf1afb02016b7b489daaf13f25412
Randy Paffenroth <randy.paffenroth@gmail.com>
2026-02-26 12:11:20 -0500

    Getting order of tests to work

--- scripts/generatedata_local.py
+++ scripts/generatedata_local.py
@@ -1,15 +1,2 @@
-import pathlib
-import generatedata
-from generatedata.data_generators import generate_all
-
-
-def main(all=False):
-    base_dir = pathlib.Path(generatedata.__path__[0])
-    data_dir = base_dir / '../data/processed'
-    generate_all(data_dir, all=all)
-
-def test_main(all=False):
-    main()
-
-if __name__ == "__main__":
-    main(all=True)
+def test_main(generatedata_local_data):
+    assert generatedata_local_data.exists()
