f7eece2aa1cdef216fa223dea26f080255cae76c
Randy Paffenroth <randy.paffenroth@gmail.com>
2026-02-27 05:42:38 -0500

    Generate local was messed up by copilot

--- scripts/generatedata_local.py
+++ scripts/generatedata_local.py
@@ -1,2 +1,15 @@
-def test_main(generatedata_local_data):
-    assert generatedata_local_data.exists()
+import pathlib
+import generatedata
+from generatedata.data_generators import generate_all
+
+
+def main(all=False):
+    base_dir = pathlib.Path(generatedata.__path__[0])
+    data_dir = base_dir / '../data/processed'
+    generate_all(data_dir, all=all)
+
+def test_main(all=False):
+    main()
+
+if __name__ == "__main__":
+    main(all=True)
